638f539c06ad82cbb841038d265abf8a17aeede6
someone <someone@DESKTOP-R729TAU>
2025-07-15 23:38:50 -0600

    Agregar archivos de proyecto.

---
+++ MrBhop/f_principal.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class f_principal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(f_principal))
+        Me.cbx_act = New System.Windows.Forms.CheckBox()
+        Me.pl_title = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lb_close = New System.Windows.Forms.Label()
+        Me.lb_min = New System.Windows.Forms.Label()
+        Me.lb_titulo = New System.Windows.Forms.Label()
+        Me.lb_settings = New System.Windows.Forms.Label()
+        Me.pl_title.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cbx_act
+        '
+        Me.cbx_act.AutoSize = True
+        Me.cbx_act.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.cbx_act.Font = New System.Drawing.Font("Verdana", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.cbx_act.ForeColor = System.Drawing.Color.White
+        Me.cbx_act.Location = New System.Drawing.Point(57, 100)
+        Me.cbx_act.Name = "cbx_act"
+        Me.cbx_act.Size = New System.Drawing.Size(106, 29)
+        Me.cbx_act.TabIndex = 0
+        Me.cbx_act.Text = "Enable"
+        Me.cbx_act.UseVisualStyleBackColor = True
+        '
+        'pl_title
+        '
+        Me.pl_title.BackColor = System.Drawing.Color.FromArgb(CType(CType(67, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(110, Byte), Integer))
+        Me.pl_title.Controls.Add(Me.PictureBox1)
+        Me.pl_title.Controls.Add(Me.lb_close)
+        Me.pl_title.Controls.Add(Me.lb_min)
+        Me.pl_title.Controls.Add(Me.lb_titulo)
+        Me.pl_title.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pl_title.Location = New System.Drawing.Point(0, 0)
+        Me.pl_title.Name = "pl_title"
+        Me.pl_title.Size = New System.Drawing.Size(230, 36)
+        Me.pl_title.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.MrBhop.My.Resources.Resources.bhop_back_inverted
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(37, 36)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'lb_close
+        '
+        Me.lb_close.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lb_close.Image = Global.MrBhop.My.Resources.Resources.cross_m
+        Me.lb_close.Location = New System.Drawing.Point(196, 11)
+        Me.lb_close.Name = "lb_close"
+        Me.lb_close.Size = New System.Drawing.Size(30, 19)
+        Me.lb_close.TabIndex = 2
+        '
+        'lb_min
+        '
+        Me.lb_min.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lb_min.Image = Global.MrBhop.My.Resources.Resources.RES
+        Me.lb_min.Location = New System.Drawing.Point(154, 15)
+        Me.lb_min.Name = "lb_min"
+        Me.lb_min.Size = New System.Drawing.Size(33, 13)
+        Me.lb_min.TabIndex = 1
+        '
+        'lb_titulo
+        '
+        Me.lb_titulo.AutoSize = True
+        Me.lb_titulo.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lb_titulo.ForeColor = System.Drawing.Color.White
+        Me.lb_titulo.Location = New System.Drawing.Point(39, 7)
+        Me.lb_titulo.Name = "lb_titulo"
+        Me.lb_titulo.Size = New System.Drawing.Size(82, 23)
+        Me.lb_titulo.TabIndex = 0
+        Me.lb_titulo.Text = "Mr. Bhop"
+        '
+        'lb_settings
+        '
+        Me.lb_settings.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lb_settings.Image = Global.MrBhop.My.Resources.Resources.settings_inv
+        Me.lb_settings.Location = New System.Drawing.Point(187, 52)
+        Me.lb_settings.Name = "lb_settings"
+        Me.lb_settings.Size = New System.Drawing.Size(31, 23)
+        Me.lb_settings.TabIndex = 2
+        '
+        'f_principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(34, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(230, 200)
+        Me.Controls.Add(Me.lb_settings)
+        Me.Controls.Add(Me.pl_title)
+        Me.Controls.Add(Me.cbx_act)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(230, 200)
+        Me.MinimumSize = New System.Drawing.Size(230, 200)
+        Me.Name = "f_principal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "f_principal"
+        Me.pl_title.ResumeLayout(False)
+        Me.pl_title.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Public WithEvents cbx_act As CheckBox
+    Friend WithEvents pl_title As Panel
+    Friend WithEvents lb_close As Label
+    Friend WithEvents lb_min As Label
+    Friend WithEvents lb_titulo As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lb_settings As Label
+End Class
---
+++ MrBhop/f_principal.vb
@@ -0,0 +1,147 @@
+﻿Option Strict On
+Option Infer Off
+
+Imports System.Threading
+Imports Memory
+Imports System.Runtime.InteropServices
+
+Public Class f_principal
+    Private Shared ReadOnly Memory As New Mem()
+    Private Shared ReadOnly Jump As String = "client.dll+0x757DF0"
+    Private Shared ReadOnly Air As String = "client.dll+0x6E1008"
+
+    Private processId As Integer = 0
+    Private bhopEnabled As Boolean = False
+    Private bhopThread As Thread
+    Private Delegate Sub BhopDelegate()
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetAsyncKeyState(vKey As Integer) As Short
+    End Function
+
+    Private Sub Bhop()
+        While bhopEnabled AndAlso cbx_act.Checked
+            If GetAsyncKeyState(Keys.F7) < 0 Then
+                bhopEnabled = False
+                Exit While
+            End If
+            If cbx_act.Checked AndAlso GetAsyncKeyState(Keys.Space) < 0 Then
+                Dim resultado As Integer = Memory.ReadInt(Air)
+                If resultado = 0 Then
+                    Memory.WriteMemory(Jump, "int", "5")
+                    Thread.Sleep(35)
+                    Memory.WriteMemory(Jump, "int", "4")
+                End If
+            End If
+            Thread.Sleep(1)
+        End While
+    End Sub
+
+    Private Sub StartBhopThread()
+        If bhopThread Is Nothing OrElse Not bhopThread.IsAlive Then
+            bhopEnabled = True
+            bhopThread = New Thread(AddressOf Bhop) With {.IsBackground = True}
+            bhopThread.Start()
+        End If
+    End Sub
+
+    Private Sub StopBhopThread()
+        bhopEnabled = False
+        If bhopThread IsNot Nothing AndAlso bhopThread.IsAlive Then
+            bhopThread.Join(100)
+        End If
+    End Sub
+
+    Private Sub cbx_act_CheckedChanged(sender As Object, e As EventArgs) Handles cbx_act.CheckedChanged
+        If cbx_act.Checked Then
+            processId = Memory.GetProcIdFromName("left4dead2")
+            If processId > 0 Then
+                Memory.OpenProcess(processId)
+                StartBhopThread()
+            Else
+                cbx_act.Checked = False
+            End If
+        Else
+            StopBhopThread()
+        End If
+    End Sub
+
+#Region "formulario"
+    Private Sub lb_min_Click(sender As Object, e As EventArgs) Handles lb_min.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub lb_close_Click(sender As Object, e As EventArgs) Handles lb_close.Click
+        StopBhopThread()
+        Me.Close()
+    End Sub
+
+
+    ' moving from title bar
+    Private mouseOffset As Point
+    Private Sub pl_title_MouseDown(sender As Object, e As MouseEventArgs) Handles pl_title.MouseDown, PictureBox1.MouseDown, lb_titulo.MouseDown
+        Dim control As Control = CType(sender, Control)
+        mouseOffset = control.PointToScreen(New Point(e.X, e.Y))
+        mouseOffset.X -= Me.Location.X
+        mouseOffset.Y -= Me.Location.Y
+    End Sub
+    Private Sub pl_title_MouseMove(sender As Object, e As MouseEventArgs) Handles pl_title.MouseMove, PictureBox1.MouseMove, lb_titulo.MouseMove
+        If e.Button = MouseButtons.Left Then
+            Dim newLocation As Point = Control.MousePosition
+            newLocation.X -= mouseOffset.X
+            newLocation.Y -= mouseOffset.Y
+            Me.Location = newLocation
+        End If
+    End Sub
+
+    ' I just was testing runtime control creation 
+    Private Sub lb_settings_Click(sender As Object, e As EventArgs) Handles lb_settings.Click
+        Dim formSize As New Size(160, 160)
+        Dim fontColor As Color = Color.White
+        Dim fontStyle As New Font(Me.cbx_act.Font.FontFamily, 10, Me.cbx_act.Font.Style)
+
+        Dim form As New Form With {
+            .Size = formSize,
+            .MaximumSize = formSize,
+            .MinimumSize = formSize,
+            .BackColor = Me.BackColor,
+            .FormBorderStyle = FormBorderStyle.FixedToolWindow,
+            .StartPosition = FormStartPosition.CenterScreen,
+            .TopMost = True,
+            .Text = "Configuration"
+        }
+
+        Dim lb_act As New Label With {
+            .Text = "Activate Bhop",
+            .ForeColor = fontColor,
+            .Font = fontStyle,
+            .Location = New Point(14, 10)
+        }
+        form.Controls.Add(lb_act)
+
+        Dim lb_deact As New Label With {
+            .Text = "Deactivate Bhop",
+            .ForeColor = fontColor,
+            .Font = fontStyle,
+            .Location = New Point(14, 64)
+        }
+        form.Controls.Add(lb_deact)
+
+        Dim tb_act As New TextBox With {
+            .Location = New Point(14, 36),
+            .Size = New Size(50, 20),
+            .Text = "F8"
+        }
+        form.Controls.Add(tb_act)
+
+        Dim tb_deact As New TextBox With {
+            .Location = New Point(14, 90),
+            .Size = New Size(50, 20),
+            .Text = "F7"
+        }
+        form.Controls.Add(tb_deact)
+
+        form.ShowDialog()
+    End Sub
+#End Region
+End Class
